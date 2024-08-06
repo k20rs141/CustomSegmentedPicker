eed527b4925a88c2a3faffc70910ea68d23a775a
Yamada Koki <koki.yamada@link-u.co.jp>
2024-08-07 00:25:50 +0900

    Add: Enumの追加

--- CustomSegmentedPicker/CustomSegmentedPicker/ContentView.swift
+++ CustomSegmentedPicker/CustomSegmentedPicker/ContentView.swift
@@ -1,5 +1,11 @@
 import SwiftUI
 
+enum TabType: String, CaseIterable, Hashable {
+    case normal = "通常"
+    case limitedTime = "期間限定"
+    case completed = "達成済み"
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
